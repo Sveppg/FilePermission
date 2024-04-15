d48aa19b0da7d39b89361db3f8108124b088bcf0
Sveppg <terminator12327@gmail.com>
2024-04-15 17:48:32 +0200

    new install script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+echo "fetching the git repo..."
+git clone https://github.com/Sveppg/FilePermisson.git
 
 cd /FilePermissions-git/build
+make
 
 echo "creating alias..."
 alias ChangePerm="./executable"
 chmod +x executable
 echo "Your command will be ChangePerm"
 echo "Add a File as a Programm parameter"
-
+ChangePerm
